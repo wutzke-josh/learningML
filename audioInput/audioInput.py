658d0432643bab5f019c9d5e1372b27fd4d97d0e
wutzke-josh <wutzke.josh@gmail.com>
2019-11-12 23:11:29 -0700

    Couldn't get pause to work so made a flashing notifier

--- audioInput/audioInput.py
+++ audioInput/audioInput.py
@@ -2,31 +2,30 @@ import alsaaudio, time
 
 import matplotlib.pyplot as plt
 
-print("listening...")
-time.sleep(.2)
+import os
 
 audio = alsaaudio.PCM(alsaaudio.PCM_CAPTURE)
 audio.setformat(alsaaudio.PCM_FORMAT_U16_LE)
 audio.setrate(8000)
-audio.setperiodsize(800)
+audio.setperiodsize(80)
+# audio.pause(1)
+os.system("clear")
 
-sounds = []
+# I'm not sure what an appropriate extenstion is...
+with open("helloSamples", "wb") as sounds:
+    for i in range(20):
+        print("again")
+        time.sleep(.2)
+        os.system("clear")
+        for j in range(100):
+            # audio.pause(0)
+            l, raw = audio.read()
+            # audio.pause(1)
+            sounds.write(raw)
 
-for i in range(40):
-    l, raw = audio.read()
-    data = [0 for i in range(l)]
-    for i in range(l):
-        data[i] = (raw[i*2] + (raw[(i * 2) + 1] << 8))
-    sounds.extend(data)
 
 
-# data = [0 for i in range(l)]
-# for i in range(100):
-#     for i in range(80):
-#         sounds.pop(0)
-#     l, raw = audio.read()
-#     for i in range(l):
-#         data[i] = (raw[i*2] + (raw[(i * 2) + 1] << 8))
-#     sounds.extend(data)
-plt.plot(sounds)
-plt.show()
+### This code will probably be useful to unpack the bytestream when opening the files for training
+# for i in range(l):
+#     data[i] = (raw[i*2] + (raw[(i * 2) + 1] << 8))
+# sounds.extend(data)
